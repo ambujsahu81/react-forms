cdac3950755188265fe202a784fd878d9ec2816b
ambujsahu81 <Ambuj.sahu1998@gmail.com>
2023-02-25 15:02:26 +0530

    adds some forms example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,32 @@
+import * as React from "react";
+import { useForm } from "react-hook-form";
 
-import { useEffect, useRef } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-
-type Inputs = {
-  example: string,
-  exampleRequired: string,
+type FormData = {
+  firstName: string;
+  lastName: string;
 };
 
-export default function Home() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
-  const renderCount = useRef(0);
-
-  useEffect(() => {
-    renderCount.current++;
-  }, []);
-
+export default function App() {
+  const { register, setValue, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const onSubmit = handleSubmit(data => console.log(data));
+  // firstName and lastName will have correct type
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <>
-      <h1>{renderCount.current}</h1>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <input type="submit" />
+    <form onSubmit={onSubmit}>
+      <label>First Name</label>
+      <input {...register("firstName")} />
+      <label>Last Name</label>
+      <input {...register("lastName")} />
+      <button
+        type="button"
+        onClick={() => {
+          setValue("lastName", "luo"); // ✅
+          setValue("firstName", true); // ❌: true is not string
+          errors.bill; // ❌: property bill does not exist
+        }}
+      >
+        SetValue
+      </button>
     </form>
-    </>
   );
 }
